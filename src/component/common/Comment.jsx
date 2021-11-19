84568e7a01b433e0415f5709d5f71bb354059549
SaaaHoP <qkrtjdgh97@gmail.com>
2021-11-19 23:30:21 +0900

    Feature: 댓글 컴포넌트 1차 작성
    #6

---
+++ src/component/common/Comment.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import styled from '@emotion/styled';
+
+const CommentWrapper = styled.div``;
+
+const CommentLineWrapper1 = styled.div``;
+
+const CommentName = styled.div``;
+
+const CommentMoreButton = styled.div``;
+
+const CommentTime = styled.div``;
+
+const CommentLineWrapper2 = styled.div``;
+
+const CommentContents = styled.div``;
+
+const CommentLineWrapper3 = styled.div``;
+
+const CommentLikeButton = styled.div``;
+
+const CommentNumber = styled.div``;
+
+const Comment = () => {
+  return <div>123</div>;
+};
+
+export default Comment;
