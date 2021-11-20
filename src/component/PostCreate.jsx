57b7aa98d4501e69d47401b4c099f47847017126
SaaaHoP <qkrtjdgh97@gmail.com>
2021-11-21 02:19:08 +0900

    Feat: DropDown 공통 컴포넌트 구현, PostCreate 수정
    #27
    #6

--- src/component/PostCreate.jsx
+++ src/component/PostCreate.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import styled from '@emotion/styled';
 import Button from './common/Button';
+import DropDown from './common/DropDown';
 
 const PostCreateWrapper = styled.div`
   display: flex;
@@ -22,54 +23,6 @@ const PostCreateExitButton = styled.div`
     height: 28px;
   }
 `;
-
-const PostCreateCategory = styled.div`
-  margin-top: 10px;
-  padding-left: 10px;
-  padding-right: 10px;
-  height: 40px;
-  border: 1px solid #828282;
-  border-radius: 8px;
-
-  font-weight: 500;
-  font-size: 16px;
-
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-
-  cursor: pointer;
-
-  img {
-    width: 32px;
-    height: 32px;
-  }
-
-  &:hover {
-    background: rgba(244, 255, 193, 0.3);
-  }
-`;
-
-const PostCreateCategoryContentsBox = styled.div`
-  width: 96%;
-  position: absolute;
-  height: 110px;
-  border: 1px solid #828282;
-  box-shadow: 0px 2px 10px rgba(0, 0, 0, 0.1);
-  border-radius: 8px;
-
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-`;
-
-const PostCreateCategoryFree = styled.div`
-  border-bottom: 1px solid #828282;
-`;
-const PostCreateCategoryInformation = styled.div`
-  border-bottom: 1px solid #828282;
-`;
-const PostCreateCategoryPromotion = styled.div``;
 
 const PostCreateTitleInput = styled.input`
   margin-top: 10px;
@@ -116,10 +69,11 @@ const PostCreateImgUploadButton = styled.div`
   }
 `;
 
-const PostCreateContentInput = styled.input`
+const PostCreateContentInput = styled.textarea`
+  resize: none;
+
   margin-top: 7px;
-  padding-left: 10px;
-  padding-right: 10px;
+  padding: 10px 10px 0 10px;
   height: 320px;
   border: 1px solid #828282;
   border-radius: 8px;
@@ -132,40 +86,28 @@ const PostCreateContentInput = styled.input`
     text-align: left;
     vertical-align: initial;
   }
+
+  :focus {
+    outline: none;
+  }
 `;
 
 const PostCreate = () => {
-  const [categoryOn, setCategoryOn] = useState(false);
-
-  const onClickCategoryOn = () => {
-    setCategoryOn(!categoryOn);
-  };
+  const list = ['자유게시판', '정보게시판', '홍보게시판'];
 
   return (
     <PostCreateWrapper>
       <PostCreateExitButton>
         <img src="/images/icons/close_grey.png" alt="close" />
       </PostCreateExitButton>
-      <PostCreateCategory onClick={onClickCategoryOn}>
-        게시판 선택
-        <img src="/images/icons/drop_down.png" alt="drop_down" />
-      </PostCreateCategory>
-      {categoryOn && (
-        <PostCreateCategoryContentsBox>
-          <PostCreateCategoryFree>자유게시판</PostCreateCategoryFree>
-          <PostCreateCategoryInformation>
-            정보게시판
-          </PostCreateCategoryInformation>
-          <PostCreateCategoryPromotion>홍보게시판</PostCreateCategoryPromotion>
-        </PostCreateCategoryContentsBox>
-      )}
+      <DropDown name="게시판 선택" list={list} />
       <PostCreateTitleInput placeholder="제목을 입력해주세요" />
       <PostCreateImgWrapper></PostCreateImgWrapper>
       <PostCreateImgUploadButton>
         <img src="/images/icons/img_box_fill.png" alt="img" />
         이미지 업로드
       </PostCreateImgUploadButton>
-      <PostCreateContentInput type="text" placeholder="내용을 입력해주세요" />
+      <PostCreateContentInput placeholder="내용을 입력해주세요" />
       <Button footer>등록</Button>
     </PostCreateWrapper>
   );
