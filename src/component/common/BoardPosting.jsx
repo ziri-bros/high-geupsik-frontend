5340b0bf7c5443f7afe339c1169adff625ac7f01
Sangmin <23mean@naver.com>
2021-11-21 00:03:03 +0900

    Fix: 예시 DB밖으로빼기
    #17

--- src/component/common/BoardPosting.jsx
+++ src/component/common/BoardPosting.jsx
@@ -2,97 +2,96 @@ import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import BoardComponent from './BoardComponent';
 
+// 예시 DB
+const examplePost = {
+  title: '제목입니다',
+  time: '21/7/23 11:34',
+  view: '100',
+  content: '점심메뉴 추천 좀',
+  images: ['/images/icons/square.png', '/images/icons/square.png'],
+  like: 5,
+  liked: true,
+  totalCommentCount: 4,
+  isMe: true,
+  comments: [
+    {
+      name: '익명1',
+      time: '21/7/23 11:50',
+      content: '냉모밀 먹어라',
+      goodCount: 0,
+      cocomments: [],
+    },
+    {
+      name: '익명2',
+      time: '21/7/23 11:50',
+      content: '치킨 먹어라',
+      goodCount: 2,
+      cocomments: [
+        {
+          name: '익명(글쓴이)',
+          time: '21/7/23 11:50',
+          content: '추천 고마워',
+          goodCount: 0,
+          cocommentsCount: 1,
+        },
+        {
+          name: '익명2',
+          time: '21/7/23 11:50',
+          content: '맛있게 먹어',
+          goodCount: 1,
+          cocommentsCount: 0,
+        },
+      ],
+    },
+  ],
+};
+//
+// 예시 DB 2
+const examplePost2 = {
+  title: '다른거 테스트',
+  time: '21/11/11 11:11',
+  view: '100',
+  content: '시발련아',
+  images: ['/images/icons/square.png', '/images/icons/square.png'],
+  like: 5,
+  liked: true,
+  totalCommentCount: 4,
+  isMe: true,
+  comments: [
+    {
+      name: '익명1',
+      time: '21/7/23 11:50',
+      content: '냉모밀 먹어라',
+      goodCount: 0,
+      cocomments: [],
+    },
+    {
+      name: '익명2',
+      time: '21/7/23 11:50',
+      content: '치킨 먹어라',
+      goodCount: 2,
+      cocomments: [
+        {
+          name: '익명(글쓴이)',
+          time: '21/7/23 11:50',
+          content: '추천 고마워',
+          goodCount: 0,
+          cocommentsCount: 1,
+        },
+        {
+          name: '익명2',
+          time: '21/7/23 11:50',
+          content: '맛있게 먹어',
+          goodCount: 1,
+          cocommentsCount: 0,
+        },
+      ],
+    },
+  ],
+};
+
 const BoardPosting = ({ path }) => {
   const [boardType, setBoardType] = useState(path);
-
-  // 예시 DB
-  const examplePost = {
-    title: '제목입니다',
-    time: '21/7/23 11:34',
-    view: '100',
-    content: '점심메뉴 추천 좀',
-    images: ['/images/icons/square.png', '/images/icons/square.png'],
-    like: 5,
-    liked: true,
-    totalCommentCount: 4,
-    isMe: true,
-    comments: [
-      {
-        name: '익명1',
-        time: '21/7/23 11:50',
-        content: '냉모밀 먹어라',
-        goodCount: 0,
-        cocomments: [],
-      },
-      {
-        name: '익명2',
-        time: '21/7/23 11:50',
-        content: '치킨 먹어라',
-        goodCount: 2,
-        cocomments: [
-          {
-            name: '익명(글쓴이)',
-            time: '21/7/23 11:50',
-            content: '추천 고마워',
-            goodCount: 0,
-            cocommentsCount: 1,
-          },
-          {
-            name: '익명2',
-            time: '21/7/23 11:50',
-            content: '맛있게 먹어',
-            goodCount: 1,
-            cocommentsCount: 0,
-          },
-        ],
-      },
-    ],
-  };
-  //
-  // 예시 DB 2
-  const examplePost2 = {
-    title: '다른거 테스트',
-    time: '21/11/11 11:11',
-    view: '100',
-    content: '시발련아',
-    images: ['/images/icons/square.png', '/images/icons/square.png'],
-    like: 5,
-    liked: true,
-    totalCommentCount: 4,
-    isMe: true,
-    comments: [
-      {
-        name: '익명1',
-        time: '21/7/23 11:50',
-        content: '냉모밀 먹어라',
-        goodCount: 0,
-        cocomments: [],
-      },
-      {
-        name: '익명2',
-        time: '21/7/23 11:50',
-        content: '치킨 먹어라',
-        goodCount: 2,
-        cocomments: [
-          {
-            name: '익명(글쓴이)',
-            time: '21/7/23 11:50',
-            content: '추천 고마워',
-            goodCount: 0,
-            cocommentsCount: 1,
-          },
-          {
-            name: '익명2',
-            time: '21/7/23 11:50',
-            content: '맛있게 먹어',
-            goodCount: 1,
-            cocommentsCount: 0,
-          },
-        ],
-      },
-    ],
-  };
-  //
 
   useEffect(() => {
     if (path === '/board') setBoardType('board');
