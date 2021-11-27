b1ce85604eb044b0181e38b3c767a8490aab66f4
kimsanglok <evertree6031@gmail.com>
2021-11-27 22:50:10 +0900

    Feat: 사용자정보 조회API 로드하는 함수 분리 resolved :#44

--- src/store/userInfo.jsx
+++ src/store/userInfo.jsx
@@ -4,9 +4,7 @@ const GET_USER_INFO = 'userInfo/GET_USER_INFO';
 
 export const getUserInfo = createAction(GET_USER_INFO);
 
-const initialState = {
-  info: null,
-};
+const initialState = {};
 
 const userInfo = handleActions({
   [GET_USER_INFO]: (state, { payload: info }) => ({
