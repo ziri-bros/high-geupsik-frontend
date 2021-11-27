98a4e047406d141c6c30d1a04ceb46d9a1661993
kimsanglok <evertree6031@gmail.com>
2021-11-27 22:55:48 +0900

    Feat: DOM render 전, 사용자정보 전역으로 저장하기 resolved :#44

--- src/index.js
+++ src/index.js
@@ -7,8 +7,31 @@ import { composeWithDevTools } from 'redux-devtools-extension';
 import { Provider } from 'react-redux';
 import App from './App';
 import rootReducer from './store';
+import { getUserInfo } from './store/userInfo';
+import { getCurrentUserInfo } from './lib/api/auth';
 
 const store = createStore(rootReducer, composeWithDevTools());
+
+function loadUser() {
+  try {
+    const user = localStorage.getItem('ACCESS_TOKEN');
+
+    if (!user) return; // 사용자가 로그인이 안된 경우
+
+    const loadAPI = async () => {
+      const response = await getCurrentUserInfo();
+
+      // 사용자가 ROLE_USER인 경우
+      if (response.status === 200) {
+        store.dispatch(getUserInfo(response.data.data));
+      }
+    };
+    loadAPI();
+  } catch (e) {
+    console.log('localstorage is not working!');
+  }
+}
+loadUser();
 
 ReactDOM.render(
   <Provider store={store}>
