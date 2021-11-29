f8d7b041028bc76ac5220bf933f76699de143fe1
Sangmin <23mean@naver.com>
2021-11-30 01:56:48 +0900

    Fix: 백엔드 서버주소 ec2로 수정 및 예시DB 삭제

--- src/component/common/Preview.jsx
+++ src/component/common/Preview.jsx
@@ -157,50 +157,6 @@ const RecentPostWrapper = styled.div`
   margin: 0 0 10px;
 `;
 
-// 예시 DB
-const examplePost = {
-  title: '제목입니다',
-  time: '21/7/23 11:34',
-  view: '100',
-  content: '점심메뉴 추천 좀',
-  images: ['/images/icons/square.png', '/images/icons/square.png'],
-  like: 5,
-  liked: true,
-  totalCommentCount: 4,
-  isMe: true,
-  comments: [
-    {
-      name: '익명1',
-      time: '21/7/23 11:50',
-      content: '냉모밀 먹어라',
-      goodCount: 0,
-      cocomments: [],
-    },
-    {
-      name: '익명2',
-      time: '21/7/23 11:50',
-      content: '치킨 먹어라',
-      goodCount: 2,
-      cocomments: [
-        {
-          name: '익명(글쓴이)',
-          time: '21/7/23 11:50',
-          content: '추천 고마워',
-          goodCount: 0,
-          cocommentsCount: 1,
-        },
-        {
-          name: '익명2',
-          time: '21/7/23 11:50',
-          content: '맛있게 먹어',
-          goodCount: 1,
-          cocommentsCount: 0,
-        },
-      ],
-    },
-  ],
-};
-
 const Preview = ({ type }) => (
   <>
     {type === 'schoolfood' && (
@@ -280,7 +236,7 @@ const Preview = ({ type }) => (
           </IconItem>
         </TitleWrapper>
         <RecentPostWrapper>
-          <BoardComponent objects={examplePost} />
+          <BoardComponent />
         </RecentPostWrapper>
       </PreviewItem>
     )}
