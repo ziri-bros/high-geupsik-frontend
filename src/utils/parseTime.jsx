e6aacaa6121e370442d48413c8bb85e33d83dfe6
SaaaHoP <qkrtjdgh97@gmail.com>
2021-11-29 23:02:42 +0900

    Feat: parseTime 수정
    #56

--- src/utils/parseTime.jsx
+++ src/utils/parseTime.jsx
@@ -1,2 +1,7 @@
 export const parseTime = time =>
-  time.toString().replace(/-/g, '/').split('T').join(' ');
+  time
+    .toString()
+    .replace(/-/g, '/')
+    .split('T')
+    .join(' ')
+    .substring(2, time.length - 3);
