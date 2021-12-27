2684cb7f21432a978dbfd919905c4f1b0df401f2
SaaaHoP <qkrtjdgh97@gmail.com>
2021-12-28 00:11:55 +0900

    Feat: BoardNavigation 페이지 별, 게시글 별 포커스 추가
    #78

--- src/component/common/HeaderNavigation.jsx
+++ src/component/common/HeaderNavigation.jsx
@@ -1,5 +1,5 @@
 import styled from '@emotion/styled';
-import { React, useEffect } from 'react';
+import React from 'react';
 import { css } from '@emotion/react';
 import { Link, useHistory } from 'react-router-dom';
 
