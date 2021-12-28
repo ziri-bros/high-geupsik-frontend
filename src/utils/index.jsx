c72fd7456c19323aaa7153a5d6a25715b46ba97b
Sangmin <23mean@naver.com>
2021-12-28 15:44:06 +0900

    Chore: env파일 수정사항 변경
    #35

--- src/utils/index.jsx
+++ src/utils/index.jsx
@@ -11,13 +11,13 @@ export const getWeekIdx = () => {
   return [1 - today.getDay(), 7 - today.getDay()];
 };
 
-export const getTargetDate = (target) => {
+export const getTargetDate = target => {
   let today = new Date();
   today = new Date(today.setDate(today.getDate() + target - 1));
 
   const year = today.getFullYear();
-  const month = (`0${today.getMonth() + 1}`).slice(-2);
-  const day = (`0${today.getDate()}`).slice(-2);
+  const month = `0${today.getMonth() + 1}`.slice(-2);
+  const day = `0${today.getDate()}`.slice(-2);
 
   return `${year}${month}${day}`;
 };
@@ -26,8 +26,8 @@ export const getTodayDate = () => {
   const today = new Date();
 
   const year = today.getFullYear();
-  const month = (`0${today.getMonth() + 1}`).slice(-2);
-  const day = (`0${today.getDate()}`).slice(-2);
+  const month = `0${today.getMonth() + 1}`.slice(-2);
+  const day = `0${today.getDate()}`.slice(-2);
 
   return `${year}${month}${day}`;
 };
