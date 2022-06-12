b7830669e523e2983ecc98490041049ceb629412
Sung Ho Park <qkrtjdgh97@gmail.com>
2022-06-12 22:54:00 +0900

    feat: notification reducer 추가
    #109

--- src/store/index.jsx
+++ src/store/index.jsx
@@ -1,11 +1,13 @@
 import { combineReducers } from 'redux';
 import schoolFoodList from './schoolFoodList';
 import userInfo from './userInfo';
+import notification from './notification';
 
 // 여러 리듀서들을 취합하는 코드
 const rootReducer = combineReducers({
   userInfo,
   schoolFoodList,
+  notification,
 });
 
 export default rootReducer;
